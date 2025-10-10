eb7ce38db6efa20ef9992d2859a025f95d0441e2
raphaeltake <raph.takeuchi@gmail.com>
2025-10-10 02:51:17 -0300

    Primeira versão calculadora em Angular

---
+++ Calculadora/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CalculadoraComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Calculadora';
+}
