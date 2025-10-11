d69bb6927530432e365ba7e748c40170620aa211
raphaeltake <raph.takeuchi@gmail.com>
2025-10-11 03:00:01 -0300

    Componente lista de compras 03 - tabela dos produtos dinâmica (@for e ngFor)

--- Lista-Compras/src/app/lista-compras/lista-compras.component.ts
+++ Lista-Compras/src/app/lista-compras/lista-compras.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [ FormsModule ],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
   item: string = ''
+  lista: ItemLista[] = []
 
   adicionarItem(){
-    console.log("item recebido: ", this.item)
+    let itemLista = new ItemLista()
+    itemLista.nome = this.item
+    itemLista.id = this.lista.length + 1
+
+    this.lista.push(itemLista)
+
     this.item = ''
   }
 }
