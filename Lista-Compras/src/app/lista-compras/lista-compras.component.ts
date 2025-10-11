a091408908c7897485df847e1eb4ec94ce7d7dea
raphaeltake <raph.takeuchi@gmail.com>
2025-10-11 02:28:57 -0300

    Componente lista de compras 02 - criação do formulário

--- Lista-Compras/src/app/lista-compras/lista-compras.component.ts
+++ Lista-Compras/src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+  item: string = ''
 
+  adicionarItem(){
+    console.log("item recebido: ", this.item)
+    this.item = ''
+  }
 }
