9c5c103ac8b220b88f73e11f9eacaab06a68b803
raphaeltake <raph.takeuchi@gmail.com>
2025-12-09 00:57:22 -0300

    Capturando parametros de URL, atualizando registro

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -8,21 +8,49 @@ import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [ FlexLayoutModule, MatCardModule, FormsModule,MatFormFieldModule, MatInputModule, MatIcon, MatButtonModule],
+  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatButtonModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
   cliente: Cliente = Cliente.newCliente()
+  atualizando: boolean = false
 
-  constructor(private service: ClienteService){
-
+  constructor(
+    private service: ClienteService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
   }
 
-  salvar(){
-    this.service.salvar(this.cliente)
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((query: any) => {
+      const params = query['params']
+      const id = params['id']
+      if (id) {
+
+        let clienteEncontrado = this.service.buscarClientePorId(id)
+        if (clienteEncontrado) {
+          this.atualizando = true
+          this.cliente = clienteEncontrado
+        }
+      }
+    })
+  }
+
+
+
+  salvar() {
+    if (!this.atualizando) {
+      this.service.salvar(this.cliente)
+      this.cliente = Cliente.newCliente()
+    } else {
+      this.service.atualizar(this.cliente)
+      this.router.navigate(['consulta'])
+    }
   }
 }
--- crud-angular-material/src/app/cliente.service.ts
+++ crud-angular-material/src/app/cliente.service.ts
@@ -16,6 +16,16 @@ export class ClienteService {
     localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage))
   }
 
+  atualizar(cliente: Cliente){
+    const storage = this.obterStorage()
+    storage.forEach(c => {
+      if(c.id === cliente.id){
+        Object.assign(c, cliente)
+      }
+    })
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage))
+  }
+
   pesquisarClientes(nomeBusca: string) : Cliente[]{
     const clientes = this.obterStorage()
     if (!nomeBusca){
@@ -23,6 +33,11 @@ export class ClienteService {
     }
 
     return clientes.filter(cliente => cliente.nome?.indexOf(nomeBusca) !== -1 )
+  }
+
+  buscarClientePorId(id: string) : Cliente | undefined{
+    const clientes = this.obterStorage()
+    return clientes.find(cliente => cliente.id === id)
   }
 
   private obterStorage() : Cliente[] {
