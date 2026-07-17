3b9b31998c39a2ac27ba442ff3514b0776d1878a
raphaeltake <raph.takeuchi@gmail.com>
2026-07-17 14:39:27 -0300

    Calculadora Angular (2.0)

---
+++ Calculadora/src/app/calculadora/calculadora.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [FormsModule],
+  templateUrl: './calculadora.html',
+  styleUrl: './calculadora.scss',
+})
+export class Calculadora {
+
+  numero1: number = 0
+  numero2: number = 0
+  resultado: number = 0
+
+  calcularResultado() {
+    this.resultado = this.numero1 + this.numero2
+  }
+
+}
