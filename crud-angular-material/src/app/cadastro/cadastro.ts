73c5e42574ee0427d64f66f32a8f074e0099dcca
raphaeltake <raph.takeuchi@gmail.com>
2026-07-22 00:40:28 -0300

    Salvar registros de usuários no localStorage do navegador

--- crud-angular-material/src/app/cadastro/cadastro.ts
+++ crud-angular-material/src/app/cadastro/cadastro.ts
@@ -4,6 +4,10 @@ import { MatCardModule } from '@angular/material/card'
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { Cliente } from './cliente'
+import { ClienteService } from '../cliente-service'
 
 @Component({
   selector: 'app-cadastro',
@@ -13,8 +17,21 @@ import { MatInputModule } from '@angular/material/input'
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
-export class Cadastro { }
+export class Cadastro {
+
+  cliente: Cliente = Cliente.newCliente()
+
+  constructor(private service: ClienteService) { }
+
+  salvar() {
+    this.service.salvar(this.cliente)
+  }
+
+
+}
---
+++ crud-angular-material/src/app/cliente-service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './cadastro/cliente';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClienteService {
+
+  static REPO_CLIENTES = "_CLIENTES"
+
+  obterStorage(): Cliente[] {
+    const repositoriosClientes = localStorage.getItem(ClienteService.REPO_CLIENTES)
+    if (repositoriosClientes) {
+      const clientes: Cliente[] = JSON.parse(repositoriosClientes)
+      return clientes
+    }
+
+    const clientes: Cliente[] = []
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes))
+    return clientes
+  }
+
+
+  salvar(cliente: Cliente) {
+    const storage = this.obterStorage()
+    storage.push(cliente)
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage))
+  }
+}
