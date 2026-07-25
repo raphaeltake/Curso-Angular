268fc8e972fa644f31689dc099f9b7e55a933597
raphaeltake <raph.takeuchi@gmail.com>
2026-07-24 22:36:00 -0300

    Json-server, para simular um backend. Salvando dados por API

--- passeio-app/src/app/app-module.ts
+++ passeio-app/src/app/app-module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { App } from './app';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withFetch()),
   ],
   bootstrap: [App]
 })
--- passeio-app/src/app/categorias/categoria/categoria.ts
+++ passeio-app/src/app/categorias/categoria/categoria.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { CategoriaService } from '../categoria-service';
 
 @Component({
   selector: 'app-categoria',
@@ -10,7 +11,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 export class Categoria {
 
   camposForm: FormGroup
-  constructor() {
+  constructor(private service: CategoriaService) {
     this.camposForm = new FormGroup({
       nome: new FormControl("", Validators.required),
       descricao: new FormControl("", Validators.required)
@@ -18,8 +19,23 @@ export class Categoria {
   }
 
   salvar() {
-    console.log(this.camposForm.value)
-    console.log(this.camposForm.valid)
+    this.camposForm.markAllAsTouched()
+
+    if (this.camposForm.valid) {
+      this.service.salvar(this.camposForm.value).subscribe({
+        next: categoria => {
+          this.camposForm.reset()
+          console.log('Salva com sucesso!', categoria)
+        },
+        error: erro => console.error("Ocorreu um erro: ", erro)
+      })
+      
+    }
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo)
+    return campo?.invalid && campo?.touched && campo?.errors?.['required'] || false
   }
 
 }
